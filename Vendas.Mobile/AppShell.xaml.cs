1a4080b8dc66bacd65081e16472d1364c3ca1379
leodviana <damasceno.leonardo@gmail.com>
2023-06-09 16:37:53 -0300

    arquitetura do projeto

---
+++ Vendas.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Vendas.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Vendas.Mobile/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Vendas.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
